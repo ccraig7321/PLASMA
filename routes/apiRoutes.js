1c7afd63c94574552d41ce86c31d938543983448
Susan Holland <segh@fastmail.com>
2020-07-18 18:08:37 -0400

    Added GET route for all users

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,14 @@ module.exports = function(app) {
   // TODO? // Passport authentication middleware and login POST route
   // Signup POST route to create new user
   // Logout GET Route
+
   // GET route for all users
+  app.get("/api/users", function(req, res) {
+    db.User.findAll({}).then(function(userData) {
+      res.json(userData);
+    });
+  });
+
   // GET route for all playlists from specific user
   // GET route for all songs from specific playlist
   // POST route for create new playlist
