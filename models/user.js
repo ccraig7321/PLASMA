b44ffc4a5bec489965da9b86e7300c1f59f6971a
Susan Holland <segh@fastmail.com>
2020-07-18 11:26:21 -0400

    Wrote User model with username and password columns

---
+++ models/user.js
@@ -0,0 +1,22 @@
+// TODO? // Need to require bcrypt if using password hashing
+
+// User model
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    // TODO? // Need method to check unhashed password in user input compared to hashed password stored in database
+
+    // TODO? // Need hook to automatically hash password before User is created
+
+    return User;
+};
