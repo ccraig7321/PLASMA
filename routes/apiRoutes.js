014a2e8ea8687dff232fc3d24c76caff1a57c9b2
Susan Holland <segh@fastmail.com>
2020-07-19 10:20:49 -0400

    eslinting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -57,6 +57,33 @@ module.exports = function(app) {
     });
   });
 
+  // POST route to find or create new genre
+  app.post("/api/genres", function(req, res) {
+    db.Genre.findOrCreate(req.body).then(function(genre) {
+      res.json(genre);
+    });
+  });
+
+  // POST route to find or create new artist
+  app.post("/api/artists", function(req, res) {
+    db.Artist.findOrCreate(req.body).then(function(artist) {
+      res.json(artist);
+    });
+  });
+
+  // POST route to find or create new song
+  app.post("/api/songs", function(req, res) {
+    db.Song.findOrCreate(req.body).then(function(song) {
+      res.json(song);
+    });
+  });
+
   // POST route to create new playlistSong
+  app.post("/api/playlistSongs", function(req, res) {
+    db.PlaylistSong.create(req.body).then(function(playlistSong) {
+      res.json(playlistSong);
+    });
+  });
+
   // DELETE route to delete playlistSong
 };
