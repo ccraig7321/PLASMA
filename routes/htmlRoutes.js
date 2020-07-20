0f484e2a91b5c297bd529b856b7f18f8ebc6cb6c
Ben Acker <bda5464@nyu.edu>
2020-07-20 10:47:39 -0400

    revsied html routes to include dashboard

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,7 +12,13 @@ module.exports = function(app) {
   app.get("/signup", function(req, res) {
     res.render("signup", {});
   });
+
+  app.get("/dashboard", function(req, res) {
+    res.render("dashboard", {});
+  });
 };
+
+
 
 
 
